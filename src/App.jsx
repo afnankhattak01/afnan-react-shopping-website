e1ef8262093b66d23fdd01efeeddc8bd3279dac1
Afnan Khattak <dev.reactx@gmail.com>
2023-12-27 13:52:33 +0500

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Preheader from "./components/preheader.jsx";
+
+function App() {
+
+    return (<>
+        <div className={"pre-header"}>
+            <Preheader/>
+        </div>
+    </>)
+}
+
+export default App
---
+++ src/components/preheader.jsx
@@ -0,0 +1,26 @@
+const PreHeader = () => {
+
+    return (<div className={"container flex justify-between"}>
+
+
+        <div className={"social-icons"}>
+            <a href={"##"}><i className="bi bi-facebook"></i></a>
+            <a href={"https://twitter.com/"}><i className="bi bi-twitter"></i></a>
+            <a href={"https://www.instagram.com/"}><i className={"bi bi-instagram"}></i></a>
+            <a href={"https://www.youtube.com/"}><i className={"bi bi-youtube"}></i></a>
+            <a href={"https://www.linkedin.com/"}><i className={"bi bi-linkedin"}></i></a>
+
+        </div>
+        <div className={"right-child-pre-header"}>
+
+            <a href={"##"}><i className="bi bi-person-fill"></i>Log-in</a>
+            <a href={"##"}><i className="bi bi-pencil-square"></i>Register Now</a>
+            <a href={"##"}><i className="bi bi-cart-fill"></i>0 Items - ($0.00)</a>
+
+
+        </div>
+    </div>)
+
+}
+
+export default PreHeader;
